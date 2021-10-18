730f8ac55b26159046ae2c8216437d6f8724d4f8
jsmendozap <jsmendozap@unal.edu.co>
2021-10-17 23:32:47 -0500

    Agregar autenticación backend

--- server.js
+++ server.js
@@ -3,11 +3,27 @@ import { conectarBD } from './db/db.js'
 import dotenv from 'dotenv'
 import Cors from 'cors'
 import rutasUsuarios from './views/usuarios/rutas.js'
+import jwt from 'express-jwt'
+import jwks from 'jwks-rsa'
 
 dotenv.config({patch: './.env'})
 const app = Express() // Crea servidor 
 
 app.use(Cors()) // soluciona error de cors de la consola del front
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://mintinc-manufacture.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'api-autenticacion-manufacture',
+  issuer: 'https://mintinc-manufacture.us.auth0.com/',
+  algorithms: ['RS256']
+});
+
+app.use(jwtCheck)
 app.use(Express.json()) // Convierte las solicitudes entrantes en formato json en un objeto manipulable 
 app.use(rutasUsuarios)
 
