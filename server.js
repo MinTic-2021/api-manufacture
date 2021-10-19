23c84d0c025e796cd9933214187fd5b60b42d8f2
Karen Ramirez Moreno <kdramirezm@gmail.com>
2021-10-18 23:47:14 -0500

    agregar rutas backend para productos

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { conectarBD } from './db/db.js'
 import dotenv from 'dotenv'
 import Cors from 'cors'
 import rutasUsuarios from './views/usuarios/rutas.js'
+import rutasProductos from './views/productos/rutas.js'
 
 dotenv.config({patch: './.env'})
 const app = Express() // Crea servidor 
@@ -10,6 +11,7 @@ const app = Express() // Crea servidor
 app.use(Cors()) // soluciona error de cors de la consola del front
 app.use(Express.json()) // Convierte las solicitudes entrantes en formato json en un objeto manipulable 
 app.use(rutasUsuarios)
+app.use(rutasProductos)
 
 const main = () => {
     return app.listen(process.env.PORT, () =>{ // Establece el puerto de escucha de solicitudes
