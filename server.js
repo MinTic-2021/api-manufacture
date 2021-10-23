fb5a580bca8777ff7e809fac4d985244b1d2cfcb
jsmendozap <jsmendozap@unal.edu.co>
2021-10-23 16:53:04 -0500

    corregir puerto

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ import dotenv from 'dotenv'
 import Cors from 'cors'
 import rutasUsuarios from './views/usuarios/rutas.js'
 import rutasVentas from './views/ventas/rutasVentas.js'
-//import jwt from 'express-jwt'
-//import jwks from 'jwks-rsa'
+import jwt from 'express-jwt'
+import jwks from 'jwks-rsa'
 import rutasProductos from './views/productos/rutas.js'
 
 dotenv.config({patch: './.env'})
+
+const port = process.env.PORT || 5000
+
 const app = Express() // Crea servidor 
 
 app.use(Cors()) // soluciona error de cors de la consola del front
 
-/*var jwtCheck = jwt({
+var jwtCheck = jwt({
     secret: jwks.expressJwtSecret({
         cache: true,
         rateLimit: true,
@@ -25,15 +28,15 @@ app.use(Cors()) // soluciona error de cors de la consola del front
   algorithms: ['RS256']
 });
 
-app.use(jwtCheck)*/
+app.use(jwtCheck)
 app.use(Express.json()) // Convierte las solicitudes entrantes en formato json en un objeto manipulable 
 app.use(rutasUsuarios)
 app.use(rutasVentas)
 app.use(rutasProductos)
 
 const main = () => {
-    return app.listen(process.env.PORT, () =>{ // Establece el puerto de escucha de solicitudes
-    console.log(`Escuchando puerto ${process.env.PORT}`)
+    return app.listen(port, () =>{ // Establece el puerto de escucha de solicitudes
+    console.log(`Escuchando puerto ${port}`)
     })
 }
 
